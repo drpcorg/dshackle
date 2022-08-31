d7ba956b54931c6380bb23bffd9bcad5ecb11491
Maxksim Fomenkov <maksim.fomenkov@p2p.org>
2022-08-31 03:07:23 +0300

    checks improvement + comments

--- src/main/kotlin/io/emeraldpay/dshackle/upstream/bitcoin/BitcoinBlockValidator.kt
+++ src/main/kotlin/io/emeraldpay/dshackle/upstream/bitcoin/BitcoinBlockValidator.kt
@@ -10,60 +10,91 @@ import io.emeraldpay.dshackle.upstream.toHexString
 import org.apache.commons.codec.binary.Hex
 import org.slf4j.LoggerFactory
 import java.math.BigInteger
-import java.util.concurrent.atomic.AtomicReference
+import java.util.concurrent.ConcurrentHashMap
 
 class BitcoinBlockValidator : BlockValidator {
+
+    private val targetsCache = ConcurrentHashMap<String, BigInteger>()
+    private val worksCache = ConcurrentHashMap<String, BigInteger>()
+
     companion object {
         private val log = LoggerFactory.getLogger(BitcoinBlockValidator::class.java)
 
-        private val targetRef = AtomicReference<ChainTargetState>()
+        private const val BITS = "bits"
+        private const val HASH = "hash"
+        private const val TIME = "time"
+        private const val NONCE = "nonce"
+        private const val HEIGHT = "height"
+        private const val VERSION = "version"
+        private const val CHAIN_WORK = "chainwork"
+        private const val MERKLE_ROOT = "merkleroot"
+        private const val PREV_BLOCK_HASH = "previousblockhash"
 
         private val MAX_HASH = BigInteger.ONE.shiftLeft(256)
-        private const val DAY = 60L * 60L * 24L
-        private const val TWO_WEEKS = 14L * DAY
+
+        /**
+         * Block target difficulty is being recalculated every 2016 blocks
+         */
+        private const val DIFFICULTY_CHANGE_PERIOD = 2016L
     }
 
     override fun isValid(currentHead: BlockContainer?, newHead: BlockContainer): Boolean =
         newHead.getParsed(Map::class.java)?.let { data ->
-            val time = data["time"] as Number
-            val bits = data["bits"] as String
-            val hash = data["hash"] as String
+            val curData = currentHead?.getParsed(Map::class.java)
+            val bits = data[BITS] as String
+            val hash = data[HASH] as String
 
             val hashValid = validateHash(bits, hash, data)
             if (!hashValid) {
                 log.warn("Invalid header hash for block {}", hash)
             }
 
-            val bitsValid = validateBits(bits, time.toLong())
-            if (!bitsValid) {
-                log.warn("Invalid bits for block {}. Too early to change difficulty target", hash)
+            val difficultyValid = validateDifficultyAdjustment(curData, data)
+            if (!difficultyValid) {
+                log.warn("Invalid difficulty for block {}. Adjustment not allowed", hash)
             }
 
-            val target = checkUpdateChainTarget(bits, time.toLong())
+            val target = calculateTarget(bits)
             val powValid = validatePoW(target, hash)
             if (!powValid) {
                 log.warn("Invalid PoW for block {}", hash)
             }
 
-            val workValid = currentHead?.let { prev ->
-                prev.getParsed(Map::class.java)?.let {
-                    val work = MAX_HASH.divide(target.add(BigInteger.ONE))
-                    validateChainWork(work, it["chainwork"] as String, data["chainwork"] as String)
-                } ?: false
-            } ?: true
+            val work = calculateWork(bits)
+            val workValid = validateChainWork(work, curData, data)
             if (!workValid) {
                 log.warn("Invalid chainwork for block {}", hash)
             }
 
-            hashValid && bitsValid && powValid && workValid
+            hashValid && difficultyValid && powValid && workValid
         } ?: false
 
+    /**
+     * Calculates and caches the difficulty target
+     */
+    private fun calculateTarget(bits: String): BigInteger =
+        targetsCache.computeIfAbsent(bits.lowercase()) { readCompactForm(bits.toLong(16)) }
+
+    /**
+     * Calculates the estimated work for the new block generation based in the target difficulty
+     *
+     * work = 2^256 / (target + 1)
+     */
+    private fun calculateWork(bits: String): BigInteger =
+        worksCache.computeIfAbsent(bits.lowercase()) {
+            val target = calculateTarget(it)
+            MAX_HASH.divide(target.add(BigInteger.ONE))
+        }
+
+    /**
+     * Checks if block hash is properly calculated for given block content
+     */
     private fun validateHash(bits: String, hash: String, data: Map<*, *>): Boolean {
-        val version = data["version"] as Number
-        val prev = data["previousblockhash"] as String
-        val merkleRoot = data["merkleroot"] as String
-        val time = data["time"] as Number
-        val nonce = data["nonce"] as Number
+        val version = data[VERSION] as Number
+        val prev = data[PREV_BLOCK_HASH] as String
+        val merkleRoot = data[MERKLE_ROOT] as String
+        val time = data[TIME] as Number
+        val nonce = data[NONCE] as Number
 
         val headerBytes = version.toLong().asUInt32()
             .plus(fromHexString(prev).reversedArray())
@@ -75,32 +106,59 @@ class BitcoinBlockValidator : BlockValidator {
         return headerBytes.sha256().sha256().reversedArray().toHexString().lowercase() == hash
     }
 
-    private fun validatePoW(target: BigInteger, hash: String): Boolean {
-        val h = BigInteger(hash, 16)
-        return h <= target
-    }
-
-    private fun validateChainWork(work: BigInteger, prevChainWork: String, chainWork: String): Boolean {
-        return BigInteger(chainWork, 16) == BigInteger(prevChainWork, 16) + work
-    }
-
-    private fun checkUpdateChainTarget(bits: String, time: Long): BigInteger =
-        targetRef.updateAndGet { old ->
-            old?.takeIf { it.raw.equals(bits, ignoreCase = true) } ?: ChainTargetState(
-                bits,
-                readCompactForm(bits.toLong(16)),
-                time,
-                old != null
-            )
-        }.target
-
-    private fun validateBits(bits: String, time: Long) =
-        targetRef.get()?.let { it.raw == bits || !it.tracked || time - it.time > TWO_WEEKS - DAY } ?: true
+    /**
+     * Checks if block hash is less than the target difficulty
+     */
+    private fun validatePoW(target: BigInteger, hash: String): Boolean =
+        BigInteger(hash, 16) <= target
 
     /**
+     * Checks if bitcoin chainwork calculated properly for the new block, which means:
+     * chainwork = prev chainwork + work
+     */
+    private fun validateChainWork(work: BigInteger, prevData: Map<*, *>?, data: Map<*, *>): Boolean =
+        prevData?.let { prev ->
+            val prevHeight = (prev[HEIGHT] as Number).toLong()
+            val prevChainWork = prev[CHAIN_WORK] as String
+            val newHeight = (data[HEIGHT] as Number).toLong()
+            val chainWork = data[CHAIN_WORK] as String
+
+            val prevGen = prevHeight / DIFFICULTY_CHANGE_PERIOD
+            val newGen = newHeight / DIFFICULTY_CHANGE_PERIOD
+            val distance = newHeight - prevHeight
+            val totalWork = if (distance == 1L) {
+                work
+            } else if (prevGen == newGen) {
+                work.multiply(distance.toBigInteger())
+            } else {
+                val blocks = LongArray(distance.toInt()) { prevHeight + it + 1 }
+                val prevWork = calculateWork(prev[BITS] as String)
+                val adjBlock = blocks.first { it % DIFFICULTY_CHANGE_PERIOD == 0L }
+                blocks.sumOf { if (it < adjBlock) prevWork else work }
+            }
+            distance > 0L && BigInteger(chainWork, 16) == BigInteger(prevChainWork, 16) + totalWork
+        } ?: true
+
+    /**
+     * Checks if difficulty adjustment is done in time (2016 blocks after the previous one)
+     * https://wiki.bitcoinsv.io/index.php/Target
+     */
+    private fun validateDifficultyAdjustment(prevData: Map<*, *>?, data: Map<*, *>): Boolean =
+        prevData?.let {
+            val prevBits = prevData[BITS] as String
+            val newBits = data[BITS] as String
+            val prevHeight = (prevData[HEIGHT] as Number).toLong()
+            val newHeight = (data[HEIGHT] as Number).toLong()
+
+            prevBits == newBits || prevHeight / DIFFICULTY_CHANGE_PERIOD < newHeight / DIFFICULTY_CHANGE_PERIOD
+        } ?: true
+
+    /**
+     * Calculates the difficulty target based on 'compact form' representation
+     *
      * https://developer.bitcoin.org/reference/block_chain.html#target-nbits
      */
-    fun readCompactForm(bits: Long): BigInteger {
+    private fun readCompactForm(bits: Long): BigInteger {
         val size: Int = (bits shr 24).toInt() and 0xFF
         if (size == 0) {
             return BigInteger.ZERO
@@ -115,11 +173,4 @@ class BitcoinBlockValidator : BlockValidator {
         val result = BigInteger(bytes)
         return if (isNegative) result.negate() else result
     }
-
-    data class ChainTargetState(
-        val raw: String,
-        val target: BigInteger,
-        val time: Long,
-        val tracked: Boolean
-    )
 }
--- src/test/groovy/io/emeraldpay/dshackle/upstream/bitcoin/BitcoinBlockValidatorSpec.groovy
+++ src/test/groovy/io/emeraldpay/dshackle/upstream/bitcoin/BitcoinBlockValidatorSpec.groovy
@@ -5,9 +5,10 @@ import spock.lang.Specification
 
 class BitcoinBlockValidatorSpec extends Specification {
     def extractor = new ExtractBlock()
+    def validator = new BitcoinBlockValidator()
 
     def "test validate block hash"() {
-        def validator = new BitcoinBlockValidator()
+        setup:
         def path = "src/test/resources/blocks/${file}.json"
         def blockContainer = extractor.extract(FileUtils.readFileToByteArray(new File(path)))
 
@@ -15,14 +16,17 @@ class BitcoinBlockValidatorSpec extends Specification {
         validator.isValid(null, blockContainer) == expect
 
         where:
-        file              || expect
-        "btc_block_valid" || true
-        "btc_block_sec_1" || true
-        "btc_block_sec_2" || true
+        file               || expect
+        "btc_block_valid"  || true
+        "btc_block_749945" || true
+        "btc_block_749951" || true
+        "btc_block_749952" || true
+        "btc_block_sec_1"  || true
+        "btc_block_sec_2"  || true
     }
 
     def "test chainwork"() {
-        def validator = new BitcoinBlockValidator()
+        setup:
         def prevPath = "src/test/resources/blocks/${prev}.json"
         def prevBlock = extractor.extract(FileUtils.readFileToByteArray(new File(prevPath)))
 
@@ -33,8 +37,26 @@ class BitcoinBlockValidatorSpec extends Specification {
         validator.isValid(prevBlock, nextBlock) == expect
 
         where:
-        prev              || next              || expect
-        "btc_block_sec_1" || "btc_block_sec_2" || true
-        "btc_block_sec_2" || "btc_block_sec_1" || false
+        prev               || next               || expect
+        "btc_block_sec_1"  || "btc_block_sec_2"  || true
+        "btc_block_749945" || "btc_block_749951" || true
+        "btc_block_sec_2"  || "btc_block_sec_1"  || false
+    }
+
+    def "test difficulty adjustment"() {
+        setup:
+        def prevPath = "src/test/resources/blocks/${prev}.json"
+        def prevBlock = extractor.extract(FileUtils.readFileToByteArray(new File(prevPath)))
+
+        def nextPath = "src/test/resources/blocks/${next}.json"
+        def nextBlock = extractor.extract(FileUtils.readFileToByteArray(new File(nextPath)))
+
+        expect:
+        validator.isValid(prevBlock, nextBlock)
+
+        where:
+        prev               || next               || expect
+        "btc_block_749951" || "btc_block_749952" || true
+        "btc_block_749945" || "btc_block_749952" || true
     }
 }
