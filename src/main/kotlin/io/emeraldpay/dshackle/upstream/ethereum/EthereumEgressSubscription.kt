83db2987ec82738d553826b0b8f6ff54b88202c9
Termina1 <terminal2010@gmail.com>
2023-08-07 20:08:49 +0300

    remove syncing subscription

--- src/main/kotlin/io/emeraldpay/dshackle/upstream/ethereum/EthereumEgressSubscription.kt
+++ src/main/kotlin/io/emeraldpay/dshackle/upstream/ethereum/EthereumEgressSubscription.kt
@@ -5,7 +5,6 @@ import io.emeraldpay.dshackle.upstream.EgressSubscription
 import io.emeraldpay.dshackle.upstream.Selector
 import io.emeraldpay.dshackle.upstream.ethereum.subscribe.ConnectLogs
 import io.emeraldpay.dshackle.upstream.ethereum.subscribe.ConnectNewHeads
-import io.emeraldpay.dshackle.upstream.ethereum.subscribe.ConnectSyncing
 import io.emeraldpay.dshackle.upstream.ethereum.subscribe.PendingTxesSource
 import io.emeraldpay.etherjar.domain.Address
 import io.emeraldpay.etherjar.hex.Hex32
@@ -24,14 +23,11 @@ open class EthereumEgressSubscription(
 
         const val METHOD_NEW_HEADS = "newHeads"
         const val METHOD_LOGS = "logs"
-        const val METHOD_SYNCING = "syncing"
         const val METHOD_PENDING_TXES = "newPendingTransactions"
     }
 
     private val newHeads = ConnectNewHeads(upstream, scheduler)
     open val logs = ConnectLogs(upstream, scheduler)
-    private val syncing = ConnectSyncing(upstream)
-
     override fun getAvailableTopics(): List<String> {
         val subs = if (upstream.getCapabilities().contains(Capability.WS_HEAD)) {
             listOf(METHOD_NEW_HEADS, METHOD_LOGS)
@@ -61,9 +57,6 @@ open class EthereumEgressSubscription(
                 return Flux.error(UnsupportedOperationException("Invalid parameter for $topic. Error: ${t.message}"))
             }
             return logs.create(paramsMap.address, paramsMap.topics).connect(matcher)
-        }
-        if (topic == METHOD_SYNCING) {
-            return syncing.connect(matcher)
         }
         if (topic == METHOD_PENDING_TXES) {
             return pendingTxesSource?.connect(matcher) ?: Flux.empty()
