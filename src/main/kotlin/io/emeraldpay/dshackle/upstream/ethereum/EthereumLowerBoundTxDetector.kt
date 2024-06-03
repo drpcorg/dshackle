f8537e09dc0e1b0050627f3f584724c00bbe820d
Кирилл <zhitelevkm@m2.ru>
2024-06-03 17:09:17 +0400

    Tx lower bound

---
+++ src/main/kotlin/io/emeraldpay/dshackle/upstream/ethereum/EthereumLowerBoundTxDetector.kt
@@ -0,0 +1,42 @@
+package io.emeraldpay.dshackle.upstream.ethereum
+
+import io.emeraldpay.dshackle.upstream.ChainRequest
+import io.emeraldpay.dshackle.upstream.Upstream
+import io.emeraldpay.dshackle.upstream.lowerbound.LowerBoundData
+import io.emeraldpay.dshackle.upstream.lowerbound.LowerBoundDetector
+import io.emeraldpay.dshackle.upstream.lowerbound.LowerBoundType
+import io.emeraldpay.dshackle.upstream.lowerbound.detector.RecursiveLowerBound
+import io.emeraldpay.dshackle.upstream.lowerbound.toHex
+import io.emeraldpay.dshackle.upstream.rpcclient.ListParams
+import reactor.core.publisher.Flux
+
+class EthereumLowerBoundTxDetector(
+    private val upstream: Upstream,
+) : LowerBoundDetector() {
+    private val recursiveLowerBound = RecursiveLowerBound(upstream, LowerBoundType.TX, setOf("No tx data"), lowerBounds)
+
+    override fun period(): Long {
+        return 3
+    }
+
+    override fun internalDetectLowerBound(): Flux<LowerBoundData> {
+        return recursiveLowerBound.recursiveDetectLowerBoundWithOffset(20) { block ->
+            upstream.getIngressReader()
+                .read(
+                    ChainRequest(
+                        "eth_getBlockTransactionCountByNumber",
+                        ListParams(block.toHex()),
+                    ),
+                )
+                .doOnNext {
+                    if (it.hasResult() && (it.getResult().contentEquals("null".toByteArray()) || it.getResultAsProcessedString().substring(2).toLong(16) == 0L)) {
+                        throw IllegalStateException("No tx data")
+                    }
+                }
+        }
+    }
+
+    override fun types(): Set<LowerBoundType> {
+        return setOf(LowerBoundType.TX)
+    }
+}
