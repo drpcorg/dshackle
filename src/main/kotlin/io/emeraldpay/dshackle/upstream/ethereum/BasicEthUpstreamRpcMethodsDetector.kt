34424ae0fea60b1c4eddef361fb373468dbc58dd
Anton <tonatoz@gmail.com>
2024-09-06 14:10:40 +0300

    eth method detector use rpc_modules

--- src/main/kotlin/io/emeraldpay/dshackle/upstream/ethereum/BasicEthUpstreamRpcMethodsDetector.kt
+++ src/main/kotlin/io/emeraldpay/dshackle/upstream/ethereum/BasicEthUpstreamRpcMethodsDetector.kt
@@ -1,5 +1,9 @@
 package io.emeraldpay.dshackle.upstream.ethereum
 
+import com.fasterxml.jackson.core.type.TypeReference
+import io.emeraldpay.dshackle.Global
+import io.emeraldpay.dshackle.upstream.ChainRequest
+import io.emeraldpay.dshackle.upstream.ChainResponse
 import io.emeraldpay.dshackle.upstream.Upstream
 import io.emeraldpay.dshackle.upstream.UpstreamRpcMethodsDetector
 import io.emeraldpay.dshackle.upstream.rpcclient.CallParams
@@ -8,9 +12,39 @@ import reactor.core.publisher.Mono
 
 // Should be Eth network only?
 class BasicEthUpstreamRpcMethodsDetector(
-    upstream: Upstream,
+    private val upstream: Upstream,
 ) : UpstreamRpcMethodsDetector(upstream) {
-    override fun detectByMagicMethod(): Mono<List<String>> = Mono.empty()
+    private val methodsToCheck = emptySet<Pair<String, CallParams>>()
 
-    override fun rpcMethods(): Set<Pair<String, CallParams>> = setOf("eth_getBlockReceipts" to ListParams("latest"))
+    override fun detectByMagicMethod(): Mono<List<String>> =
+        upstream
+            .getIngressReader()
+            .read(ChainRequest("rpc_modules", ListParams()))
+            .flatMap(ChainResponse::requireResult)
+            .map(::parseRpcModules)
+            .onErrorResume {
+                log.warn("Can't detect rpc_modules of upstream ${upstream.getId()}, reason - {}", it.message)
+                Mono.just(emptyList())
+            }
+
+    override fun rpcMethods(): Set<Pair<String, CallParams>> =
+        methodsToCheck.plus(
+            "eth_getBlockReceipts" to ListParams("latest"),
+        )
+
+    private fun parseRpcModules(data: ByteArray): List<String> {
+        val modules = Global.objectMapper.readValue(data, object : TypeReference<HashMap<String, String>>() {})
+        modules.forEach { (module, _) ->
+            //       "debug": "1.0",
+            //        "eth": "1.0",
+            //        "net": "1.0",
+            //        "rpc": "1.0",
+            //        "txpool": "1.0",
+            //        "web3": "1.0"
+//            DefaultEthereumMethods(upstream.getChain()).getGroupMethods(module).forEach {
+//                methodsToCheck.plus(it)
+//            }
+        }
+        return emptyList() // Force call detectByMethod
+    }
 }
