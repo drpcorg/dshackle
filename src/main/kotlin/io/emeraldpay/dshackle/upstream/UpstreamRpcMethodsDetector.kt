d406bf67dcf6952288c69e6951d4c8ebabb13310
Anton <tonatoz@gmail.com>
2024-09-11 15:53:36 +0300

    mv notAvailableRegexps to class variable

--- src/main/kotlin/io/emeraldpay/dshackle/upstream/UpstreamRpcMethodsDetector.kt
+++ src/main/kotlin/io/emeraldpay/dshackle/upstream/UpstreamRpcMethodsDetector.kt
@@ -14,6 +14,14 @@ abstract class UpstreamRpcMethodsDetector(
 ) {
     protected val log: Logger = LoggerFactory.getLogger(this::class.java)
 
+    private val notAvailableRegexps =
+        listOf(
+            "method ([A-Za-z0-9_]+) does not exist/is not available",
+            "([A-Za-z0-9_]+) found but the containing module is disabled",
+            "Method not found",
+            "The method ([A-Za-z0-9_]+) is not available",
+        ).map { s -> s.toRegex() }
+
     open fun detectRpcMethods(): Mono<Map<String, Boolean>> = detectByMagicMethod().switchIfEmpty(detectByMethod())
 
     protected fun detectByMethod(): Mono<Map<String, Boolean>> =
@@ -29,12 +37,7 @@ abstract class UpstreamRpcMethodsDetector(
                             .map { method to true }
                             .onErrorResume { err ->
                                 val notAvailableError =
-                                    listOf(
-                                        "method ([A-Za-z0-9_]+) does not exist/is not available",
-                                        "([A-Za-z0-9_]+) found but the containing module is disabled",
-                                        "Method not found",
-                                        "The method ([A-Za-z0-9_]+) is not available",
-                                    ).any { s -> s.toRegex().containsMatchIn(err.message ?: "") }
+                                    notAvailableRegexps.any { s -> s.containsMatchIn(err.message ?: "") }
                                 if (notAvailableError) {
                                     Mono.just(method to false)
                                 } else {
