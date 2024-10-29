1d0096662ad451fe48b324002cf9438dc8ba91bf
Anton <tonatoz@gmail.com>
2024-10-29 14:54:29 +0300

    Revert "rm ZkSyncCallLimitValidator"
    This reverts commit 08ebf2cb97fb691cdb93591518169adc0c5689b7.

--- src/main/kotlin/io/emeraldpay/dshackle/upstream/ethereum/EthereumChainSpecific.kt
+++ src/main/kotlin/io/emeraldpay/dshackle/upstream/ethereum/EthereumChainSpecific.kt
@@ -43,56 +43,57 @@ import reactor.core.scheduler.Scheduler
 import java.math.BigInteger
 
 object EthereumChainSpecific : AbstractPollChainSpecific() {
+
     private val log: Logger = LoggerFactory.getLogger(EthereumChainSpecific::class.java)
 
-    override fun parseBlock(
-        data: ByteArray,
-        upstreamId: String,
-        api: ChainReader,
-    ): Mono<BlockContainer> = Mono.just(BlockContainer.fromEthereumJson(data, upstreamId))
+    override fun parseBlock(data: ByteArray, upstreamId: String, api: ChainReader): Mono<BlockContainer> {
+        return Mono.just(BlockContainer.fromEthereumJson(data, upstreamId))
+    }
 
-    override fun getFromHeader(
-        data: ByteArray,
-        upstreamId: String,
-        api: ChainReader,
-    ): Mono<BlockContainer> = parseBlock(data, upstreamId, api)
+    override fun getFromHeader(data: ByteArray, upstreamId: String, api: ChainReader): Mono<BlockContainer> {
+        return parseBlock(data, upstreamId, api)
+    }
 
-    override fun latestBlockRequest() = ChainRequest("eth_getBlockByNumber", ListParams("latest", false))
-
-    override fun listenNewHeadsRequest(): ChainRequest = ChainRequest("eth_subscribe", ListParams("newHeads"))
-
-    override fun unsubscribeNewHeadsRequest(subId: String): ChainRequest = ChainRequest("eth_unsubscribe", ListParams(subId))
+    override fun latestBlockRequest() =
+        ChainRequest("eth_getBlockByNumber", ListParams("latest", false))
+    override fun listenNewHeadsRequest(): ChainRequest =
+        ChainRequest("eth_subscribe", ListParams("newHeads"))
+    override fun unsubscribeNewHeadsRequest(subId: String): ChainRequest =
+        ChainRequest("eth_unsubscribe", ListParams(subId))
 
     override fun localReaderBuilder(
         cachingReader: CachingReader,
         methods: CallMethods,
         head: Head,
         logsOracle: LogsOracle?,
-    ): Mono<ChainReader> = Mono.just(EthereumLocalReader(cachingReader as EthereumCachingReader, methods, head, logsOracle))
+    ): Mono<ChainReader> {
+        return Mono.just(EthereumLocalReader(cachingReader as EthereumCachingReader, methods, head, logsOracle))
+    }
 
-    override fun subscriptionBuilder(headScheduler: Scheduler): (Multistream) -> EgressSubscription =
-        { ms ->
-            val pendingTxes: PendingTxesSource =
-                (ms.getAll())
-                    .filter { it is GenericUpstream }
-                    .map { it as GenericUpstream }
-                    .filter { it.getIngressSubscription() is EthereumIngressSubscription }
-                    .mapNotNull {
-                        (it.getIngressSubscription() as EthereumIngressSubscription).getPendingTxes()
-                    }.let {
-                        if (it.isEmpty()) {
-                            NoPendingTxes()
-                        } else if (it.size == 1) {
-                            it.first()
-                        } else {
-                            AggregatedPendingTxes(it)
-                        }
+    override fun subscriptionBuilder(headScheduler: Scheduler): (Multistream) -> EgressSubscription {
+        return { ms ->
+            val pendingTxes: PendingTxesSource = (ms.getAll())
+                .filter { it is GenericUpstream }
+                .map { it as GenericUpstream }
+                .filter { it.getIngressSubscription() is EthereumIngressSubscription }
+                .mapNotNull {
+                    (it.getIngressSubscription() as EthereumIngressSubscription).getPendingTxes()
+                }.let {
+                    if (it.isEmpty()) {
+                        NoPendingTxes()
+                    } else if (it.size == 1) {
+                        it.first()
+                    } else {
+                        AggregatedPendingTxes(it)
                     }
+                }
             EthereumEgressSubscription(ms, headScheduler, pendingTxes)
         }
+    }
 
-    override fun makeCachingReaderBuilder(tracer: Tracer): CachingReaderBuilder =
-        { ms, caches, methodsFactory -> EthereumCachingReader(ms, caches, methodsFactory, tracer) }
+    override fun makeCachingReaderBuilder(tracer: Tracer): CachingReaderBuilder {
+        return { ms, caches, methodsFactory -> EthereumCachingReader(ms, caches, methodsFactory, tracer) }
+    }
 
     override fun upstreamValidators(
         chain: Chain,
@@ -102,62 +103,46 @@ object EthereumChainSpecific : AbstractPollChainSpecific() {
     ): List<SingleValidator<UpstreamAvailability>> {
         var validators = emptyList<SingleValidator<UpstreamAvailability>>()
         if (options.validateSyncing) {
-            validators +=
-                GenericSingleCallValidator(
-                    ChainRequest("eth_syncing", ListParams()),
-                    upstream,
-                ) { data ->
-                    val raw = Global.objectMapper.readTree(data)
-                    if (raw.isBoolean) {
-                        if (raw.asBoolean()) {
+            validators += GenericSingleCallValidator(
+                ChainRequest("eth_syncing", ListParams()),
+                upstream,
+            ) { data ->
+                val raw = Global.objectMapper.readTree(data)
+                if (raw.isBoolean) {
+                    if (raw.asBoolean()) {
+                        UpstreamAvailability.SYNCING
+                    } else {
+                        UpstreamAvailability.OK
+                    }
+                } else {
+                    if (raw.get("currentBlock") != null && raw.get("highestBlock") != null) {
+                        val current =
+                            BigInteger(raw.get("currentBlock")?.asText()?.lowercase()?.substringAfter("x"), 16)
+                        val highest =
+                            BigInteger(raw.get("highestBlock")?.asText()?.lowercase()?.substringAfter("x"), 16)
+
+                        if (highest - current > config.syncingLagSize.toBigInteger()) {
                             UpstreamAvailability.SYNCING
                         } else {
                             UpstreamAvailability.OK
                         }
                     } else {
-                        if (raw.get("currentBlock") != null && raw.get("highestBlock") != null) {
-                            val current =
-                                BigInteger(
-                                    raw
-                                        .get("currentBlock")
-                                        ?.asText()
-                                        ?.lowercase()
-                                        ?.substringAfter("x"),
-                                    16,
-                                )
-                            val highest =
-                                BigInteger(
-                                    raw
-                                        .get("highestBlock")
-                                        ?.asText()
-                                        ?.lowercase()
-                                        ?.substringAfter("x"),
-                                    16,
-                                )
-
-                            if (highest - current > config.syncingLagSize.toBigInteger()) {
-                                UpstreamAvailability.SYNCING
-                            } else {
-                                UpstreamAvailability.OK
-                            }
-                        } else {
-                            log.error("Received unknown syncing object ${raw.toPrettyString()} for upstream ${upstream.getId()}")
-                            UpstreamAvailability.OK
-                        }
-                    }.also {
-                        upstream.getHead().onSyncingNode(it == UpstreamAvailability.SYNCING)
+                        log.error("Received unknown syncing object ${raw.toPrettyString()} for upstream ${upstream.getId()}")
+                        UpstreamAvailability.OK
                     }
+                }.also {
+                    upstream.getHead().onSyncingNode(it == UpstreamAvailability.SYNCING)
                 }
+            }
         }
         if (options.validatePeers) {
-            validators +=
-                GenericSingleCallValidator(
-                    ChainRequest("net_peerCount", ListParams()),
-                    upstream,
-                ) { data ->
-                    val peers = Integer.decode(String(data).trim('"'))
-                    if (peers < options.minPeers) UpstreamAvailability.IMMATURE else UpstreamAvailability.OK
-                }
+            validators += GenericSingleCallValidator(
+                ChainRequest("net_peerCount", ListParams()),
+                upstream,
+            ) { data ->
+                val peers = Integer.decode(String(data).trim('"'))
+                if (peers < options.minPeers) UpstreamAvailability.IMMATURE else UpstreamAvailability.OK
+            }
         }
         return validators
     }
@@ -168,18 +153,19 @@ object EthereumChainSpecific : AbstractPollChainSpecific() {
         options: Options,
         config: ChainConfig,
     ): List<SingleValidator<ValidateUpstreamSettingsResult>> {
-        val validators =
-            mutableListOf(
-                ChainIdValidator(upstream, chain),
-                OldBlockValidator(upstream),
-            )
-        val limitValidator = EthCallLimitValidator(upstream, options, config)
+        val limitValidator = callLimitValidatorFactory(upstream, options, config, chain)
+
+        val validators = mutableListOf(
+            ChainIdValidator(upstream, chain),
+            OldBlockValidator(upstream),
+        )
         if (limitValidator.isEnabled()) {
             validators.add(limitValidator)
         }
         if (options.validateGasPrice) {
             validators.add(GasPriceValidator(upstream, config))
         }
+
         return validators
     }
 
@@ -199,19 +185,26 @@ object EthereumChainSpecific : AbstractPollChainSpecific() {
         config: UpstreamsConfig.Upstream<*>?,
     ): UpstreamRpcMethodsDetector? = config?.let { BasicEthUpstreamRpcMethodsDetector(upstream, it) }
 
-    override fun lowerBoundService(
-        chain: Chain,
-        upstream: Upstream,
-    ): LowerBoundService = EthereumLowerBoundService(chain, upstream)
+    override fun lowerBoundService(chain: Chain, upstream: Upstream): LowerBoundService {
+        return EthereumLowerBoundService(chain, upstream)
+    }
 
-    override fun finalizationDetectorBuilder(): FinalizationDetector = EthereumFinalizationDetector()
+    override fun finalizationDetectorBuilder(): FinalizationDetector {
+        return EthereumFinalizationDetector()
+    }
 
     override fun upstreamSettingsDetector(
         chain: Chain,
         upstream: Upstream,
-    ): UpstreamSettingsDetector = EthereumUpstreamSettingsDetector(upstream, chain)
+    ): UpstreamSettingsDetector {
+        return EthereumUpstreamSettingsDetector(upstream, chain)
+    }
 
-    override fun makeIngressSubscription(ws: WsSubscriptions): IngressSubscription = EthereumWsIngressSubscription(ws)
+    override fun makeIngressSubscription(ws: WsSubscriptions): IngressSubscription {
+        return EthereumWsIngressSubscription(ws)
+    }
 
-    override fun callSelector(caches: Caches): CallSelector = EthereumCallSelector(caches)
+    override fun callSelector(caches: Caches): CallSelector {
+        return EthereumCallSelector(caches)
+    }
 }
